44e6186ac488b2c630814613aa49dbc78a04ba0b
scanmonk <scanmonkgraphics@gmail.com>
2025-07-28 19:04:54 +0530

    CORS serverless fix

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
-// src/index.ts
-
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import serverless from 'serverless-http';
 
 import authRoutes from './routes/authRoutes';
 import noteRoutes from './routes/noteRoutes';
@@ -12,13 +11,16 @@ dotenv.config();
 
 const app: Express = express();
 
-app.use(cors({
-    origin: '*',
+app.use(
+  cors({
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-app.use(express.json()); 
+// Middleware
+app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
@@ -27,11 +29,12 @@ app.use('/api/notes', noteRoutes);
 
 const MONGO_URI = process.env.MONGO_URI;
 if (MONGO_URI) {
-    mongoose.connect(MONGO_URI)
-        .then(() => console.log("Successfully connected to MongoDB."))
-        .catch(err => console.error("Connection error", err));
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => console.log('Successfully connected to MongoDB.'))
+    .catch((err) => console.error('Connection error', err));
 } else {
-    console.error("FATAL ERROR: MONGO_URI is not defined.");
+  console.error('FATAL ERROR: MONGO_URI is not defined.');
 }
 
-export default app;
+export const handler = serverless(app);
