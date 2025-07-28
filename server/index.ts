c9c38249df4b22a3a56e3ff10814c1faac26a7a2
scanmonk <scanmonkgraphics@gmail.com>
2025-07-28 17:54:26 +0530

    Added corsOptions in index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@
 import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 
 import authRoutes from './routes/authRoutes';
 import noteRoutes from './routes/noteRoutes';
@@ -14,21 +14,23 @@ const app: Express = express();
 
 const allowedOrigins = [
     process.env.FRONTEND_URL, 
-    'http://localhost:5173' 
-].filter(Boolean); 
+    'http://localhost:5173'    
+];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true,
-}));
+    credentials: true, 
+};
+
+app.options('*', cors(corsOptions));
+
+app.use(cors(corsOptions));
 
 
 app.use(express.json()); 
@@ -46,6 +48,5 @@ if (MONGO_URI) {
 } else {
     console.error("FATAL ERROR: MONGO_URI is not defined.");
 }
-
 
 export default app;
