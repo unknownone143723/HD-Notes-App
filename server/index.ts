433e577c0c5ff80563c0ba4032f053c0549f3ea1
scanmonk <scanmonkgraphics@gmail.com>
2025-07-28 17:46:42 +0530

    changed index.ts according to vercel CORS

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+// src/index.ts
+
 import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -6,42 +8,44 @@ import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import noteRoutes from './routes/noteRoutes';
 
-
 dotenv.config();
 
 const app: Express = express();
 
-const frontendURL = process.env.FRONTEND_URL || "http://localhost:5173";
+const allowedOrigins = [
+    process.env.FRONTEND_URL, 
+    'http://localhost:5173' 
+].filter(Boolean); 
+
 app.use(cors({
-    origin: frontendURL
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }));
+
+
 app.use(express.json()); 
 
 // Routes
-app.get('/', (req: Request, res: Response) => {
-    res.send('Notes App API is running...');
-});
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 
 
 const MONGO_URI = process.env.MONGO_URI;
-if (!MONGO_URI) {
+if (MONGO_URI) {
+    mongoose.connect(MONGO_URI)
+        .then(() => console.log("Successfully connected to MongoDB."))
+        .catch(err => console.error("Connection error", err));
+} else {
     console.error("FATAL ERROR: MONGO_URI is not defined.");
-    process.exit(1);
 }
 
-mongoose.connect(MONGO_URI)
-    .then(() => console.log("Successfully connected to MongoDB."))
-    .catch(err => {
-        console.error("Connection error", err);
-        process.exit(1);
-    });
-
-
-// const PORT = process.env.PORT || 5001;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
 
 export default app;
